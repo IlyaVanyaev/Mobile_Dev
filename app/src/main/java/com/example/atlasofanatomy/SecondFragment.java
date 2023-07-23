4311c19dca15eb712b00a0c21cdc9d9731e8fa20
Ilya <IL4Van@yandex.ru>
2023-07-23 21:20:28 +0300

    added Fragment lifecycle displaying with toasts and logs(3)

--- app/src/main/java/com/example/atlasofanatomy/SecondFragment.java
+++ app/src/main/java/com/example/atlasofanatomy/SecondFragment.java
@@ -1,20 +1,26 @@
 package com.example.atlasofanatomy;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class SecondFragment extends Fragment {
+
+    private final String TAG = this.getClass().getSimpleName();
 
     TextView tv;
     Intent intent;
@@ -23,6 +29,9 @@ public class SecondFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.d(TAG, TAG + " onCreate");
+        Toast.makeText(getContext(), TAG + " onCreate", Toast.LENGTH_SHORT).show();
 
     }
 
@@ -49,6 +58,73 @@ public class SecondFragment extends Fragment {
             }
         });
 
+        Log.d(TAG, TAG + " onCreateView");
+        Toast.makeText(getContext(), TAG + " onCreateView", Toast.LENGTH_SHORT).show();
+
         return view;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        Log.d(TAG, TAG + " onAttach");
+        Toast.makeText(getContext(), TAG + " onAttach", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        Log.d(TAG, TAG + " onStart");
+        Toast.makeText(getContext(), TAG + " onStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        Log.d(TAG, TAG + " onResume");
+        Toast.makeText(getContext(), TAG + " onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        Log.d(TAG, TAG + " onPause");
+        Toast.makeText(getContext(), TAG + " onPause", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        Log.d(TAG, TAG + " onStop");
+        Toast.makeText(getContext(), TAG + " onStop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+        Log.d(TAG, TAG + " onDestroyView");
+        Toast.makeText(getContext(), TAG + " onDestroyView", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        Log.d(TAG, TAG + " onDestroy");
+        Toast.makeText(getContext(), TAG + " onDestroy", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+
+        Log.d(TAG, TAG + " onDetach");
+        Toast.makeText(getContext(), TAG + " onDetach", Toast.LENGTH_SHORT).show();
     }
 }
